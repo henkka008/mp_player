6755b261b06e55e8d0496bb5893f7c3cb3c01b2f
henkka008 <henkka008@hotmail.com>
2025-11-27 01:17:53 +0200

    added input function and exit

--- main.py
+++ main.py
@@ -15,6 +15,28 @@ def main():
     if not os.path.isdir(folder):
         print(f"Folder '{folder}' not found")
         return
+    mp3_files = [file for file in os.listdir(folder) if file.endswith(".mp3")]
+
+    if not mp3_files:
+        print("No .mp3 files found!")
+
+    while True:
+        print("***** MP3 PLAYER *****")
+        print("MP3 song list:")
+
+        for index, song in enumerate(mp3_files, start=1):
+            print(f"{index}. {song}")
+        
+
+        choice_input = input("\nEnter the song # to play (or 'Q' to quit): ")
+
+        if choice_input.upper() == "Q":
+            print("Exiting Player...")
+            break
+
+        if not choice_input.isdigit():
+            print("Enter a valid number")
+            continue
 
 if __name__ == "__main__":
     main()
